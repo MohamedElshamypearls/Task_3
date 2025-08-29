5f92784db574cfb135f8a8d2d481fe2d8ce30a47
moham <moham@MOHAMED>
2025-08-29 15:07:49 +0300

    Add Elshamy Class

---
+++ Task/Elshamy.cs
@@ -0,0 +1,9 @@
+﻿namespace Task
+{
+    public class Elshamy
+    {
+        public string  Name{ get; set; }
+        public string  Salary{ get; set; }
+        public string  Age{ get; set; }
+    }
+}
